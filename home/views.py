d932c1958b723882f14696b5c6c6fa86f2c67065
devfer <devfercastro@gmail.com>
2024-02-15 20:49:48 -0300

    Redirect the non-logged user to authentication view

--- home/views.py
+++ home/views.py
@@ -7,6 +7,6 @@ from datetime import datetime
 def home(request):
     return render(request, 'home/welcome.html', {'today': datetime.today()})
 
-@login_required # block the access if not login
+@login_required(login_url='/admin') # block the access if not login and redirect to authentication template
 def authorized(request):
     return render(request, 'home/authorized.html', {})
