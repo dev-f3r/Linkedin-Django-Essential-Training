59180293029636377bd24b10a5bb621bc61d9f15
devfer <devfercastro@gmail.com>
2024-02-15 19:42:44 -0300

    Passing data to the template

--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from datetime import datetime
+
 def home(request):
-    return render(request, 'home/welcome.html', {})
+    return render(request, 'home/welcome.html', {'today': datetime.today()})
