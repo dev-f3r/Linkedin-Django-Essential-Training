d0d0dd75afc5c7dd3adfd9d8e86e4fc2a367e3d2
devfer <devfercastro@gmail.com>
2024-02-15 21:43:43 -0300

    First model for notes

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,8 @@
+from typing import Any
 from django.db import models
 
-# Create your models here.
+class Notes(models.Model):
+    # * Notes fields
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
