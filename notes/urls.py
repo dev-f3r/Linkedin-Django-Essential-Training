f0174d4a4ec0721f2dd504fc7b2231ced5a7bcdb
devfer <devfercastro@gmail.com>
2024-02-19 17:50:44 -0300

    Note's app simple views replaced by class-based views

--- notes/urls.py
+++ notes/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notes/', views.list),
-    path('notes/<int:pk>', views.detail) # pk is a parameter for detail view
+    path('notes/', views.NotesListView.as_view()),
+    path('notes/<int:pk>', views.NotesDetailView.as_view()) # pk is a parameter for detail view
 ]
