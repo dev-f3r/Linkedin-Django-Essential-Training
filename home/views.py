c14e888ada82724843c37b27da9fd9e7594793af
devfer <devfercastro@gmail.com>
2024-02-19 15:08:50 -0300

    Replacing the home's app views for a class-based views

--- home/views.py
+++ home/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+# from django.contrib.auth.decorators import login_required # No longer required
 
 from datetime import datetime
 
-def home(request):
-    return render(request, 'home/welcome.html', {'today': datetime.today()})
 
-@login_required(login_url='/admin') # block the access if not login and redirect to authentication template
+class HomeView(TemplateView):
+    template_name = "home/welcome.html"
+    extra_content = {"today": datetime.today()}
+
+
+class AuthorizedView(LoginRequiredMixin, TemplateView):
+    template_name = "home/authorized.html"
+
+
+# @login_required(login_url='/admin') # block the access if not login and redirect to authentication template
 def authorized(request):
-    return render(request, 'home/authorized.html', {})
+    return render(request, "home/authorized.html", {})
