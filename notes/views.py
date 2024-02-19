458209e5a697a79bc0fb346f5f089beb509e9d54
devfer <devfercastro@gmail.com>
2024-02-19 14:43:50 -0300

    New view funtion that renders a template with the information of one note

--- notes/views.py
+++ notes/views.py
@@ -6,3 +6,8 @@ def list(request):
     all_notes = Notes.objects.all()
 
     return render(request, 'notes/notes_list.html', {'notes': all_notes})
+
+def detail(request, pk):
+    note = Notes.objects.get(pk=pk)
+
+    return render(request, 'notes/notes_details.html', {'note': note})
