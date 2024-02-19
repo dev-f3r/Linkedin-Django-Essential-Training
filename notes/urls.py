301c84f1f09dfb11c585bb580b19eb7fb7606ad5
devfer <devfercastro@gmail.com>
2024-02-19 12:16:13 -0300

    Error passing context to template

--- notes/urls.py
+++ notes/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('notes', views.list),
+    path('notes/', views.list),
 ]
--- notes/views.py
+++ notes/views.py
@@ -5,4 +5,4 @@ from .models import Notes
 def list(request):
     all_notes = Notes.objects.all()
 
-    return render(request, 'notes/notes_list.html', {'notes', all_notes})
+    return render(request, 'notes/notes_list.html', {'notes': all_notes})
