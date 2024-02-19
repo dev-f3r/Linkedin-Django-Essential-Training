17ee29587ec10db45a7b195ade22b1dd3a65fa3b
devfer <devfercastro@gmail.com>
2024-02-19 20:47:51 -0300

    New view and template for new notes

--- notes/urls.py
+++ notes/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('notes/', views.NotesListView.as_view(), name='notes.list'),
-    path('notes/<int:pk>', views.NotesDetailView.as_view(), name='notes.detail') # pk is the note's id
+    path('notes/<int:pk>', views.NotesDetailView.as_view(), name='notes.detail'), # pk is the note's id
+    path('notes/new', views.NotesCreateView.as_view(), name='notes.new')
 ]
