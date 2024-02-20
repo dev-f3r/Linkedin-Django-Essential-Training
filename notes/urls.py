0644f52a60c9652314825298d702d46f982c12f0
devfer <devfercastro@gmail.com>
2024-02-20 11:19:45 -0300

    New view to delete notes

--- notes/urls.py
+++ notes/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('notes/', views.NotesListView.as_view(), name='notes.list'),
     path('notes/<int:pk>', views.NotesDetailView.as_view(), name='notes.detail'), # pk is the note's id
     path('notes/<int:pk>/edit', views.NotesUpdateView.as_view(), name='notes.update'), # pk is the note's id
+    path('notes/<int:pk>/delete', views.NotesDeleteView.as_view(), name='notes.delete'), # pk is the note's id
     path('notes/new', views.NotesCreateView.as_view(), name='notes.new')
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import Http404
 from django.views.generic import UpdateView,CreateView, DetailView, ListView
+from django.views.generic.edit import DeleteView
 
 from .models import Notes
 from .forms import NotesForm
+
+class NotesDeleteView(DeleteView):
+    model = Notes
+    success_url = "/smart/notes"
+    template_name = "notes/notes_delete.html"
 
 class NotesUpdateView(UpdateView):
     model = Notes
