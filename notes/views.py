a03eec3f6ad3e1b9c07465c29eb2882b88eae0be
devfer <devfercastro@gmail.com>
2024-02-20 21:41:05 -0300

    Adding manually the user field to each notes before saving it

--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import UpdateView,CreateView, DetailView, ListView
 from django.views.generic.edit import DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http.response import HttpResponseRedirect
 
 from .models import Notes
 from .forms import NotesForm
@@ -19,6 +20,18 @@ class NotesCreateView(CreateView):
     model = Notes
     success_url = '/smart/notes'
     form_class = NotesForm
+
+    # Override the 'form_valid' method
+    def form_valid(self, form):
+        # Saves the form data to a model instance without commiting it to the database
+        self.object = form.save(commit=False)
+        # Sets the 'user' field of the note object to the logged user
+        self.object.user = self.request.user
+        # Saves the notel
+        self.object.save()
+
+        # Redirects to the success URL
+        return HttpResponseRedirect(self.get_success_url())
 
 class NotesListView(LoginRequiredMixin, ListView):
     model = Notes
