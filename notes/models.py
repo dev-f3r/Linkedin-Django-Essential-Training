b4d5ceb3df695e0a982b8b252dc6842726d95fea
devfer <devfercastro@gmail.com>
2024-02-20 21:03:21 -0300

    Foreignkey for notes models to associate notes with users

--- notes/models.py
+++ notes/models.py
@@ -1,8 +1,12 @@
 from typing import Any
 from django.db import models
+from django.contrib.auth.models import User
 
 class Notes(models.Model):
     # * Notes fields
     title = models.CharField(max_length=200)
     text = models.TextField()
+    # "auto_now_add" Current date
     created = models.DateTimeField(auto_now_add=True)
+    # "on_delete" If a user is delete his notes will also.
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
